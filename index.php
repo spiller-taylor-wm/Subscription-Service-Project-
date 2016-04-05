efb88cdd5929551ce989291ebc41ca565689dd5e
Start Coding Student 2 <session2@START-Coding-MBP-19.local>
2016-04-04 17:33:52 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+        <!-- Links for the navigation bar -->
+        <script type="text/javascript" rel="script" src="js/nav.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/form.css" />
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div align="center">
+            <h1>Listy</h1>
+            <a href="profile.php">profile</a>
+        </div>
+        <div class="container">
+            <div class="login-container">
+                <div id="output"></div>
+                <div class="avatar"></div>
+                <div class="form-box">
+                    <form action="" method="post">
+                        <input name="user" type="text" placeholder="username">
+                        <input type="password" placeholder="password">
+                        <button class="btn btn-info btn-block login" type="submit">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Profile</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+        <!-- Bootstrap -->
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" />
+    </head>
+    <body>
+        <script src="js/navbar.js" type="text/javascript" rel="script"></script>
+
+    </body>
+</html>
